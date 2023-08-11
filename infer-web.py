faf3e8872c0a93800dba39b05930c0d71251666a
ba1yya <34457638+Andrey5800@users.noreply.github.com>
2023-08-11 21:45:40 +0300

    44

--- infer-web.py
+++ infer-web.py
@@ -1944,19 +1944,7 @@ with gr.Blocks(title="RVC WebUI") as app:
 
 
 
-            with gr.Row():
-                ckpt_dir = gr.Textbox(label=i18n("RVC模型路径"), value="", interactive=True)
-            with gr.Row():
-                onnx_dir = gr.Textbox(
-                    label=i18n("Onnx输出路径"), value="", interactive=True
-                )
-            with gr.Row():
-                infoOnnx = gr.Label(label="info")
-            with gr.Row():
-                butOnnx = gr.Button(i18n("导出Onnx模型"), variant="primary")
-            butOnnx.click(
-                export_onnx, [ckpt_dir, onnx_dir], infoOnnx, api_name="export_onnx"
-            )
+          
 
 
 
