bf86dada98c98a686b9dadb055b16989b736abbc
ba1yya <34457638+Andrey5800@users.noreply.github.com>
2023-08-14 14:03:25 +0300

    Update kaggle.py

--- kaggle.py
+++ kaggle.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-source_dir = '/kaggle/input/ljkglkjlrs33'
+source_dir = '/kaggle/input/kjfklla3322'
 target_dir = '/opt/conda/'
 
 for root, dirs, files in os.walk(source_dir):
