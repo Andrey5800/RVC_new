6e104bc42f3d7ffbf71cb7e6418d34654ef94d41
ba1yya <34457638+Andrey5800@users.noreply.github.com>
2023-08-14 10:22:47 +0300

    Create kaggle.py

---
+++ kaggle.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+source_dir = '/kaggle/input/lkjalgkdfjg4444444'
+target_dir = '/opt/conda/'
+
+for root, dirs, files in os.walk(source_dir):
+
+  for filename in files:
+
+    # Формирование путей
+    relative_path = os.path.relpath(root, source_dir)
+    source_path = os.path.join(root, filename)
+    target_path = os.path.join(target_dir, relative_path, filename)
+
+    # Копирование файла, если не существует    
+    if not os.path.exists(target_path):
+      os.makedirs(os.path.dirname(target_path), exist_ok=True)
+      shutil.copy2(source_path, target_path)
+
+  # Копирование папок
+  for dir_name in dirs:
+    
+    source_path = os.path.join(root, dir_name)  
+    relative_path = os.path.relpath(source_path, source_dir)
+    target_path = os.path.join(target_dir, relative_path)
+
+    if not os.path.exists(target_path):
+      os.makedirs(target_path)
